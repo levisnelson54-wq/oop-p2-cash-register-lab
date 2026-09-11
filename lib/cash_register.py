06dfbccf8261bc5aa7b6b5b6df2485b7679f92f7
levisnelson54-wq <levisnelson54@gmail.com>
2026-09-11 03:22:14 +0300

    Complete cash register lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,54 @@
 #!/usr/bin/env python3
 
+
 class CashRegister:
-  pass
+
+    def __init__(self, discount=0):
+        self.discount = discount
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+        self.items.extend([item] * quantity)
+
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if self.discount == 0:
+            print("There is no discount to apply.")
+            return
+
+        discount_amount = self.total * (self.discount / 100)
+        self.total -= discount_amount
+        display_total = int(self.total) if self.total == int(self.total) else self.total
+        print(f"After the discount, the total comes to ${display_total}.")
+
+    def void_last_transaction(self):
+        if not self.previous_transactions:
+            print("There is no transaction to void.")
+            return
+
+        transaction = self.previous_transactions.pop()
+        self.total -= transaction["price"] * transaction["quantity"]
+
+        for _ in range(transaction["quantity"]):
+            if transaction["item"] in self.items:
+                self.items.remove(transaction["item"])
